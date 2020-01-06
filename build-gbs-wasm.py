4b0b79e6438aef88cc86a91c4d978b5195082a26
BIOWP <ymenghank@gmail.com>
2020-01-07 02:26:32 +0800

    add releasing to web GBS script

--- build-gbs-wasm.py
+++ build-gbs-wasm.py
@@ -41,6 +41,16 @@ def main():
     p = subprocess.Popen(cmd, shell=True)
     p.wait()
 
+    # release to web GBS
+    target_dir = "../GoBattleSim/src"
+    if os.path.exists(target_dir):
+        print("releasing to web GBS ... ", end='')
+        shutil.copyfile(f"{BinDir}/GBS_Engine.js",
+                        f"{target_dir}/GBS_Engine.js")
+        shutil.copyfile(f"{BinDir}/GBS_Engine.wasm",
+                        f"{target_dir}/GBS_Engine.wasm")
+        print("done")
+
 
 if __name__ == "__main__":
     main()
